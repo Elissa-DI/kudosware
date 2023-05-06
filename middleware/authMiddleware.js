8330901b284f0b98d41e23b8caa6803d66001ae8
Elissa <elissadusabe@gmail.com>
2023-05-06 22:37:52 +0200

    added things on backend

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -23,3 +23,30 @@ const requireAuth = (req,res,next) => {
      }
 
 }
+
+const checkUser = (req,res, next) => {
+    const token = req.cookies.jwt
+
+    if(token) {
+     jwt.verify(token, 'secret', async (err, decodedToken) => {
+
+          if(err) {
+               console.log(err.message)
+               res.locals.user = null;
+               next();
+          } else {
+             console.log(decodedToken)
+             let user = await User.findById(decodedToken.id)
+
+             res.locals.user = user;
+             next()    
+          }
+
+       })
+    } else {
+     res.locals.user = null
+     next()
+ } 
+ }
+
+module.exports ={ requireAuth,checkUser }
