62759178536f87bef44b4b0ff0fb4408391b91ae
Elissa <elissadusabe@gmail.com>
2023-05-06 22:36:36 +0200

    added things on backend

---
+++ routes/authroutes.js
@@ -0,0 +1,19 @@
+const {Router} = require("express")
+const authcontrollers = require ('../controllers/authcontrollers')
+const router = Router()
+
+router.get("/signup",authcontrollers.signup_get)
+
+router.post("/signup", authcontrollers.signup_post)
+
+router.get("/login",authcontrollers.login_get)
+
+router.post("/login",authcontrollers.login_post)
+
+router.get("/logout",authcontrollers.logout_get)
+
+router.get("/resumeAdd", authcontrollers.resume_get)
+
+router.post("/resume", authcontrollers.resume_post)
+
+module.exports = router;
